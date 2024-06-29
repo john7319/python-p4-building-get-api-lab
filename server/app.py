63c0f6405f83bbb996fb0d3b876f39d18c06342b
JohnKaranja <jonniekaras@gmail.com>
2024-06-29 23:02:24 +0300

    completed tasks

--- server/app.py
+++ server/app.py
@@ -20,19 +20,30 @@ def index():
 
 @app.route('/bakeries')
 def bakeries():
-    return ''
+    bakeries = [bakery.to_dict() for bakery in Bakery.query.all()]
+    response = make_response(bakeries, 200, {"Content-Type": "application/json"})
+    return response
 
 @app.route('/bakeries/<int:id>')
 def bakery_by_id(id):
-    return ''
+    backery = Bakery.query.filter(Bakery.id == id).first()
+    backery_dict = backery.to_dict()
+    response = make_response(backery_dict, 200, {"Content-Type": "application/json"})
+    return response
 
 @app.route('/baked_goods/by_price')
 def baked_goods_by_price():
-    return ''
+    backed_goods = BakedGood.query.order_by(BakedGood.price.desc()).all()
+    baked_goods_list = [baked_good.to_dict() for baked_good in backed_goods]
+    response = make_response(baked_goods_list, 200, {"Content-Type": "application/json"})
+    return response
 
 @app.route('/baked_goods/most_expensive')
 def most_expensive_baked_good():
-    return ''
+    backed_goods = BakedGood.query.order_by(BakedGood.price.desc()).limit(1).first()
+    backed_goods_dict = backed_goods.to_dict()
+    response = make_response(backed_goods_dict, 200)
+    return response
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
